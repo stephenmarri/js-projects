cdea416d5e613b736040515ed077514b68d6eb73
steve <stephenmarri7@gmail.com>
2024-08-14 11:45:00 -0500

    added icons and some other changes

--- tmp/src/Components/Copyright.jsx
+++ tmp/src/Components/Copyright.jsx
@@ -6,11 +6,19 @@ const Copyright = () => {
         <div className=
         {`${style.copyright_container} fr`}
         >
-            <span>Copyright © 2024 Terrebone Pediatrics Limited</span>
-            <span>
-                fb
-            </span>
+            <div className={style.copyright_text}>Copyright © 2024 Terrebone Pediatrics Limited</div>
+            <div className=
+            {`${style.copyright_icons} fr`}
+            >
+                <a href='mailto:example@domain.com' style={{backgroundImage: `url('/icons/s3/mail.svg')`}} className={`${style.icon_fb} ${style.media_icons} media_icons` } target="_blank" ></a>                  
+                <a href='tel:+11012023003' style={{backgroundImage: `url('/icons/s3/phone.svg')`}} className={`${style.icon_tel} ${style.media_icons} media_icons` } target="_blank" ></a>
+                <a href='https://maps.app.goo.gl/75NY9NNvidx1S2bJ9' style={{backgroundImage: `url('/icons/s3/maps.svg')`}} className={`${style.icon_fb} ${style.media_icons} media_icons` } target="_blank" ></a>
+                <a href='https://www.facebook.com/childrenwecare2017' style={{backgroundImage: `url('/icons/s3/fb.svg')`}} className={`${style.icon_tel} ${style.media_icons} media_icons` } target="_blank" ></a>
+                
+            </div>
         </div>
+
+        
     )
 }
 
