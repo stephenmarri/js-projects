df80e2857e394308d5b2b8b284eddfa3b49f7494
steve <stephenmarri7@gmail.com>
2024-08-16 11:51:23 -0500

    coming soon page added

--- tmp/src/App.jsx
+++ tmp/src/App.jsx
@@ -2,17 +2,24 @@ import { Outlet } from "react-router-dom";
 import Copyright from "./Components/Copyright"
 import Navbar from "./Pages/Navbar/Navbar";
 import { Analytics } from '@vercel/analytics/react';
+import ComingSoon from "./Pages/Coming Soon/ComingSoon";
 
 function App() {
 
+  // return (
+  //   <>
+  //     <div id="main_container">
+  //       <Analytics />
+  //       <Navbar />
+  //       <Outlet />
+  //       <Copyright />
+  //     </div>
+  //   </>
+  // )
   return (
-    <>
-      <div id="main_container">
+    <>      
         <Analytics />
-        <Navbar />
-        <Outlet />
-        <Copyright />
-      </div>
+        <ComingSoon />
     </>
   )
 }
---
+++ tmp/src/Pages/Coming Soon/ComingSoon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ComingSoon.css'
+
+const ComingSoon = () => {
+    return (
+        <div class="wrapper">
+            <h1>coming soon<span class="dot"></span></h1>          
+        </div>
+    )
+}
+
+export default ComingSoon
