ec81a6e5ff61f8021c3937b19502ee7d4ad74247
steve <stephenmarri7@gmail.com>
2024-08-19 00:39:25 -0500

    backend - added vercel.json

--- wisher/backend/config/dbConn.js
+++ wisher/backend/config/dbConn.js
@@ -4,6 +4,7 @@ const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
     } catch (err) {
+        console.log("Missing Mongo URI")
         console.log(err)
     }
 }
